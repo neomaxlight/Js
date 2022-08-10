2c1098e34e9c0ba8238a5b8d42b59271cd422e89
neomaxlight <neomaxlight@gmail.com>
2022-08-10 20:33:17 +0300

    feat add html css

---
+++ js/script.js
@@ -0,0 +1,24 @@
+'use strict'
+
+//number 1, 2, +Infinity, -Infinity, 0.3, NaN
+//string 'dog' "cat", `cow`
+//bolean true, false 
+//object {} [] null
+//undefined undefined
+//null = 0
+//bigint 1n 
+//symbol {}
+//typeof null=> 'object'
+// + - / * **(** - возведение в степень)
+// var let const
+
+
+let numFirst = 1;
+numFirst = "fjgfgkfkgf";
+
+var dog = 'Bim'
+dog = 1
+console.log(dog)
+
+// способ обьявления переменной
+ const num = 1
